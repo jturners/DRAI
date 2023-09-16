e7137052f523c824a3d2c643dbd3bb046d63e776
Haochen <77473582+Yologuy101@users.noreply.github.com>
2023-09-16 09:48:20 -0700

    Update ChatGPT_model.py
    added user input for testing

--- ChatGPT_model.py
+++ ChatGPT_model.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 
-# openai.organization = "org-6AKIjdbpljcerbhjbd24A2Qz"
+openai.organization = "org-6AKIjdbpljcerbhjbd24A2Qz"
 # Load your API key from an environment variable
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -14,10 +14,10 @@ def simplify(prompt, model="gpt-4"):
     messages = [
         {
             "role": "system",
-            "content": "Simpify and summarise content given by the user",
-        },
-        {"role": "user", "content": prompt},
-    ]
+            "content": "Simpify and summarise content given by the user or help user find relevent information of subject",
+        }
+        ]
+    messages.append(prompt)
     response = openai.ChatCompletion.create(
         model=model, temperature=0, messages=messages
     )
@@ -26,6 +26,9 @@ def simplify(prompt, model="gpt-4"):
 
 
 # test case
-prompt = "Meloxicam - Meloxicam is a nonsteroidal anti-inflammatory drug (NSAID) used to relieve the symptoms of arthritis (juvenile rheumatoid arthritis, osteoarthritis, and rheumatoid arthritis), such as inflammation, swelling, stiffness, and joint pain."
+#prompt = {"role": "user", "content": "Meloxicam - Meloxicam is a nonsteroidal anti-inflammatory drug (NSAID) used to relieve the symptoms of arthritis (juvenile rheumatoid arthritis, osteoarthritis, and rheumatoid arthritis), such as inflammation, swelling, stiffness, and joint pain."}
+userInput = input('Content to summarise: ')
+prompt = {"role": "user", "content": userInput}
+
 response = simplify(prompt)
 print(response)
